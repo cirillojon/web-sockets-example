05cc92bfdd176f68775908017db6afbcc743bc54
Jon <jonathancirillo@outlook.com>
2023-08-09 10:17:46 -0400

    create socket.io instance

--- server/index.js
+++ server/index.js
@@ -1,2 +1,7 @@
 // create an http server using the http module of node.js
 const http = require('http').createServer();
+
+// create an instance of socket.io and pass the http object
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
